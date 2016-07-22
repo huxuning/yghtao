d71da617b41b8da10d892cf19be04df8cd834fb9
huxuning <123794909@qq.com>
2016-07-22 21:01:07 +0800

    首页完成度90%

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 //轮播图
-function lunbo(){
+;function lunbo(){
 	var index = 0;
-	$('.bannerBtn p').eq(index).animate({width:225},4000);
-	setInterval(function(){
+	$('.bannerBtn p').eq(index).animate({width:235},4000);
+	var timer = setInterval(function(){
 		$('.bannerBtn p').eq(index).animate({width:0},0);
 		$('.banner a').eq(index).fadeOut(800);	
 		index==$('.banner a').length-1 ? index=0:index++;
-		$('.bannerBtn p').eq(index).animate({width:225},4000);
+		$('.bannerBtn p').eq(index).animate({width:235},4000);
 		$('.banner a').eq(index).fadeIn(800);
 	},4000);
 }
 lunbo();
+//点击更换banner图
+$('.bannerBtn').on('click','p',function(){
+
+});
+
+//抢购板块
